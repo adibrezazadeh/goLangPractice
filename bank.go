ed37aebbf3d1d36d326ba6a2298f8a2750fbf6b4
Adib rezazadeh <adib.rezazadeh@gmail.com>
2025-10-28 17:52:18 +0330

    complete bank

--- bank.go
+++ bank.go
@@ -1,19 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+
+)
 
 func Bank() {
-	var amountBalance float64 = 0
-	choice := menu()
-	if choice == 1 {
-		fmt.Println("Your Amount Balance Is: ", amountBalance)
-		menu()
+	var amountBalance, err = getBalanceFromFile()
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("----------------")
+		panic("Sorry Cant Continue")
+	}
+	fmt.Println("Welcome To SunCafe Bank!")
+	for {
+		choice := menu()
+		switch choice {
+		case 1:
+			fmt.Println("Your Amount Balance Is: ", amountBalance)
+		case 2:
+			var addMoney float64
+			fmt.Print("How Much You Love To Add : ")
+			fmt.Scan(&addMoney)
+			if addMoney <= 0 {
+				fmt.Println("Invalid Amount")
+				continue
+			}
+			amountBalance += addMoney
+			fmt.Println("Money Added Your new Amount Balance Is: ", amountBalance)
+			writeBalanceToFile(amountBalance)
+		case 3:
+			var withdrawMoney float64
+			fmt.Print("How Much You Love To Withdraw : ")
+			fmt.Scan(&withdrawMoney)
+			if withdrawMoney <= 0 {
+				fmt.Println("Invalid Amount")
+				continue
+			} else if withdrawMoney > amountBalance {
+				fmt.Println("Yor Request Is More Than Your Balance")
+				continue
+			}
+			amountBalance -= withdrawMoney
+			fmt.Println("Money Added Your new Amount Balance Is: ", amountBalance)
+			writeBalanceToFile(amountBalance)
+		default:
+			fmt.Println("Good By BaBy!")
+			return
+		}
+
 	}
 }
 
 func menu() int {
 	var menuChoose int
-	fmt.Println("Welcome To SunCafe Bank!")
 	fmt.Println("What Do You Want To Do ?")
 	fmt.Println("1. Check Balance")
 	fmt.Println("2. Deposit Money")
@@ -22,4 +65,21 @@ func menu() int {
 	fmt.Print("4. Your Choice: ")
 	fmt.Scan(&menuChoose)
 	return menuChoose
+}
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile("balance.txt", []byte(balanceText), 0644)
+}
+func getBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile("balance.txt")
+	if err != nil {
+		return 0, errors.New("!!Can not Find File!!")
+	}
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+	if err != nil {
+		return 0, errors.New("!!Can not Parse Balance!!")
+	}
+	return balance, nil
+
 }
