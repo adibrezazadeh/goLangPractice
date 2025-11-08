77b4876c1e9da9a3d2c5dcae5133e42a87bb87d5
adib <adib.rezazadeh@gmail.com>
2025-11-08 12:33:26 +0330

    price calculator

--- priceCalculator/price.go
+++ priceCalculator/price.go
@@ -3,5 +3,16 @@ package pricecalculator
 import "fmt"
 
 func PriceCalculator() {
-	fmt.Print("HI")
+	var prices []float64 = []float64{10, 20, 30}
+	taxRate := []float64{0, 0.7, 0.1, 0.15}
+	result := make(map[float64][]float64)
+	for _, taxRate := range taxRate {
+		var taxIncludedPrices []float64 = make([]float64, len(prices))
+		for priceIndex, price := range prices {
+			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
+
+		}
+		result[taxRate] = taxIncludedPrices
+	}
+	fmt.Println(result)
 }
